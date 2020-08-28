6d39e37c2ba17e93b09ac16c6b6de3cd7b94e458
Dennis Tang <dwtang612@gmail.com>
2020-08-28 12:44:27 -0700

    Completed 2nd Question TextWrap

---
+++ PYTHON/Textwrap.py
@@ -0,0 +1,21 @@
+# You are given a string s and width max_width.
+# Your task is to wrap the string into a paragraph of width max_width.
+# <------Sample Input------>
+# ABCDEFGHIJKLIMNOQRSTUVWXYZ
+# 4
+# 
+# <------Sample Output------>
+# ABCD
+# EFGH
+# IJKL
+# IMNO
+# QRST
+# UVWX
+# YZ
+
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+if __name__ == '__main__':
