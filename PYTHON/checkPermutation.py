37cfa74faf65ade228b5c55cb4e08bb8d3f6943d
Dennis Tang <dwtang612@gmail.com>
2020-09-26 22:38:04 -0700

    Permutation

--- PYTHON/checkPermutation.py
+++ PYTHON/checkPermutation.py
@@ -1,15 +1,20 @@
-#Given two strings, write a method to decide if one is a permutation of the other.
+#Given two strings, write a method to decide if one string contains a permutation of the other.
 
 def checkPerumutation(string1, string2):
+    answer = False
     if (len(string1) > len(string2)):
         longstring = string1
         shortstring = string2
     else:
         longstring = string2
         shortstring = string1
+    revstring = longstring[::-1]
 
-    
-            
+    for i in range(0, len(longstring) - len(shortstring)):
+        if longstring[i:i+len(shortstring)] == shortstring:
+            return True
+        elif revstring[i:i+len(shortstring)] == shortstring:
+            return True
     return False
 
 if __name__ == '__main__':
