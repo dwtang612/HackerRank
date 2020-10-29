cb6c994096e7735f0402884edb13bc4b0389f216
Dennis Tang <dwtang612@gmail.com>
2020-10-29 03:54:08 -0700

    Print_Format Complete

--- PYTHON/print_formatted.py
+++ PYTHON/print_formatted.py
@@ -1,4 +1,8 @@
 def print_formatted(number):
-    # your code goes here
+    gap = len("{0:b}".format(number))
+    for i in range(0, number):
+        print("{0:{gap}d} {0:{gap}o} {0:{gap}X} {0:{gap}b}".format(i, gap=gap))
 
 if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
